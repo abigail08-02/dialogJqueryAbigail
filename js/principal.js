f0f8df7719d218c1a11959286cc460c30ede8b0f
abigail <abigailguzman.08.02@gmail.com>
2020-10-23 19:20:32 -0500

    programar lista

--- js/principal.js
+++ js/principal.js
@@ -1,21 +1,35 @@
-$(function () {
+    $(function () {
 
-    $("#dialogEliminar").dialog({
-        autoOpen: false,
-        modal: true,
-        buttons:{
-                 "Acepto": function () {
-                     alert ("Eliminando");
-            },
-             Cancel: function () {
-                    console.log("nissdnef");
+        var celulares = [
+            "LG", "SAMSUNG", "iPhone", "HUAWEI"
+        ]
+
+        for (let i = 0; i < celulares.length; i++) {
+            const cell = celulares[i];
+            console.log(cell);
+            $("#lista-celulares").append(`
+                <li>
+
+                    ${cell}
+                    <a href="#" class="btn-eliminar">Eliminar</a>
+
+                </li>
+            `);
+        }
+
+      $("#dialogEliminar").dialog({
+            autoOpen: false,
+            modal: true,
+            buttons:{
+                    "Acepto": function () {
+                        alert ("Eliminando");
+                },
+                Cancel: function () {
+                        console.log("nissdnef");
+                    }
                 }
-            }
-    });
+      });
 
-    $(".btn-eliminar").on("click", function () {
+      $(".btn")
 
-        $("#dialogEliminar").dialog("open");
     })
-
-})
